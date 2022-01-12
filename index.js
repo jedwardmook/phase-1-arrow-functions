75ad3acfe76c3a91ffaf424b3fba5d9e67ad1075
John Mook <jedwardmook@yahoo.com>
2022-01-11 20:42:21 -0600

    commit tree f8559cc4ad1dccc40e1384becc0dfcb845004b17

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+    return 2000/100;
+}
+divide();
+
+const square = parameter1 => parameter1*parameter1;
+
+const add = (parameter1, parameter2) => parameter1+parameter2;
+
